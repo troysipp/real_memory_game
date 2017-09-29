c615e2d44a5e1d93272288efdd10c667a20363e0
594037 <troysipprelle@c02v40zghtdg.users.bah.com>
2017-09-29 15:01:52 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-console.log("Up and Running!");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = []
 
-console.log("User flipped " + cardThree);
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo;
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2); {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
